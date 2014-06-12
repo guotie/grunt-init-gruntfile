05a54ab87b8556139e28878f15179905565e9078
guotie <guotie.9@gmail.com>
2014-06-12 14:56:20 +0800

    add files

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,94 +1,159 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({{% if (min_concat) { %}
-    // Metadata.{% if (package_json) { %}
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',{% } else { %}
-    meta: {
-      version: '0.1.0'
-    },
-    banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '* http://PROJECT_WEBSITE/\n' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
-      'YOUR_NAME; Licensed MIT */\n',{% } } %}
-    // Task configuration.{% if (min_concat) { %}
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['{%= lib_dir %}/{%= file_name %}.js'],
-        dest: 'dist/{%= file_name %}.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/{%= file_name %}.min.js'
-      }
-    },{% } %}
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,{% if (dom) { %}
-        browser: true,{% } %}
-        globals: {{% if (jquery) { %}
-          jQuery: true
-        {% } %}}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['{%= lib_dir %}/**/*.js', '{%= test_dir %}/**/*.js']
-      }
-    },{% if (dom) { %}
-    {%= test_task %}: {
-      files: ['{%= test_dir %}/**/*.html']
-    },{% } else { %}
-    {%= test_task %}: {
-      files: ['{%= test_dir %}/**/*_test.js']
-    },{% } %}
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', '{%= test_task %}']
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
 
-  // These plugins provide necessary tasks.{% if (min_concat) { %}
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');{% } %}
-  grunt.loadNpmTasks('grunt-contrib-{%= test_task %}');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+         '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' + 
+         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + 
+         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', '{%= test_task %}'{%= min_concat ? ", 'concat', 'uglify'" : "" %}]);
+        // Task configuration.
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            dist: {
+                src: ['{%= lib_dir %}/{%= file_name %}.js'],
+                dest: 'dist/{%= file_name %}.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/{%= file_name %}.min.js'
+            }
+        },
 
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    jQuery: true
+                }
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib_test: {
+                src: ['{%= lib_dir %}/**/*.js', '{%= test_dir %}/**/*.js']
+            }
+        },
+
+        copy: {
+            js: {
+                src: 'dist/<%= pkg.name %>.min.js',
+                dest: 'assets/js/<%= pkg.name %>.min.js'
+            }
+        },
+
+        connect: {
+            options: {
+                port: 8000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    middleware: function(connect) {
+                        return [
+                            connect().use(
+                                '/assets',
+                                connect.static('./assets')
+                            ),
+                            connect().use(
+                                '/bower_components',
+                                connect.static('./bower_components')
+                            ),
+                            connect.static('app')
+                        ];
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    open: true,
+                    base: 'dist'
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib_test: {
+                files: '<%= jshint.lib_test.src %>',
+                tasks: ['jshint:lib_test', 'qunit']
+            },
+            less: {
+                files: ['app/less/**.less'],
+                tasks: ['less']
+            },
+            js: {
+                files: ['app/js/**.js'],
+                tasks: ['jshint', 'concat', 'uglify', 'copy:js']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    'app/{,*/}*.html',
+                    'assets/styles/{,*/}*.css',
+                    'assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'mocha', 'concat', 'uglify', 'copy', 'less']);
+
+    grunt.registerTask('serve', 'Compile then start a connect web server', function(target) {
+        if (target === 'dist') {
+            return grunt.task.run(['default', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'connect:livereload',
+            'watch'
+        ]);
+    });
 };
--- template.js
+++ template.js
@@ -13,9 +13,9 @@ exports.description = 'Create a basic Gruntfile.';
 
 // Template-specific notes to be displayed before question prompts.
 exports.notes = 'This template tries to guess file and directory paths, but ' +
-  'you will most likely need to edit the generated Gruntfile.js file before ' +
-  'running grunt. _If you run grunt after generating the Gruntfile, and ' +
-  'it exits with errors, edit the file!_';
+    'you will most likely need to edit the generated Gruntfile.js file before ' +
+    'running grunt. _If you run grunt after generating the Gruntfile, and ' +
+    'it exits with errors, edit the file!_';
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = 'Gruntfile.js';
@@ -23,86 +23,97 @@ exports.warnOn = 'Gruntfile.js';
 // The actual init template.
 exports.template = function(grunt, init, done) {
 
-  init.process({}, [
-    // Prompt for these values.
-    {
-      name: 'dom',
-      message: 'Is the DOM involved in ANY way?',
-      default: 'Y/n',
-      warning: 'Yes: QUnit unit tests + JSHint "browser" globals. No: Nodeunit unit tests.'
-    },
-    {
-      name: 'min_concat',
-      message: 'Will files be concatenated or minified?',
-      default: 'Y/n',
-      warning: 'Yes: min + concat tasks. No: nothing to see here.'
-    },
-    {
-      name: 'package_json',
-      message: 'Will you have a package.json file?',
-      default: 'Y/n',
-      warning: 'This changes how filenames are determined and banners are generated.'
-    }
-  ], function(err, props) {
-    props.dom = /y/i.test(props.dom);
-    props.min_concat = /y/i.test(props.min_concat);
-    props.package_json = /y/i.test(props.package_json);
-    props.test_task = props.dom ? 'qunit' : 'nodeunit';
-    props.file_name = props.package_json ? '<%= pkg.name %>' : 'FILE_NAME';
+    init.process({}, [
+        // Prompt for these values.
+        init.prompt('name'),
+        init.prompt('description'),
+        init.prompt('version'),
+        init.prompt('title'),
+    init.prompt('licenses', 'MIT'),
+    init.prompt('author_name'),
+    init.prompt('author_email'),
+    init.prompt('author_url'),
+    ], function(err, props) {
+        props.dom = /y/i.test(props.dom);
+        props.min_concat = true;
+        props.less = true;
+        props.package_json = true;
+        props.test_task = props.dom ? 'karma' : 'mocha';
+        props.file_name = '<%= pkg.name %>';
 
-    // Find the first `preferred` item existing in `arr`.
-    function prefer(arr, preferred) {
-      for (var i = 0; i < preferred.length; i++) {
-        if (arr.indexOf(preferred[i]) !== -1) {
-          return preferred[i];
+
+        // Find the first `preferred` item existing in `arr`.
+
+        function prefer(arr, preferred) {
+            for (var i = 0; i < preferred.length; i++) {
+                if (arr.indexOf(preferred[i]) !== -1) {
+                    return preferred[i];
+                }
+            }
+            return preferred[0];
         }
-      }
-      return preferred[0];
-    }
 
-    // Guess at some directories, if they exist.
-    var dirs = grunt.file.expand({filter: 'isDirectory'}, '*').map(function(d) { return d.slice(0, -1); });
-    props.lib_dir = prefer(dirs, ['lib', 'src']);
-    props.test_dir = prefer(dirs, ['test', 'tests', 'unit', 'spec']);
+        // Guess at some directories, if they exist.
+        var dirs = grunt.file.expand({
+            filter: 'isDirectory'
+        }, '*').map(function(d) {
+            return d.slice(0, -1);
+        });
+        console.log("dirs:", dirs);
+        props.lib_dir = prefer(dirs, ['lib', 'src', 'app']);
+        props.test_dir = prefer(dirs, ['test', 'tests', 'unit', 'spec']);
 
-    // Maybe this should be extended to support more libraries. Patches welcome!
-    props.jquery = grunt.file.expand({filter: 'isFile'}, '**/jquery*.js').length > 0;
+        console.log(props);
+        // Maybe this should be extended to support more libraries. Patches welcome!
+        props.jquery = grunt.file.expand({
+            filter: 'isFile'
+        }, '**/jquery*.js').length > 0;
 
-    // Files to copy (and process).
-    var files = init.filesToCopy(props);
+        // Files to copy (and process).
+        var files = init.filesToCopy(props);
+        console.log("files:", files)
 
-    // Actually copy (and process) files.
-    init.copyAndProcess(files, props);
+        // Actually copy (and process) files.
+        init.copyAndProcess(files, props);
+
+        // If is package_json true, generate package.json
+        var devDependencies = {
+            'grunt': '~0.4.2',
+            'grunt-contrib-jshint': '~0.7.2',
+            "grunt-contrib-concat": "~0.3.0",
+            "grunt-contrib-connect": "^0.8.0",
+            "grunt-contrib-copy": "^0.5.0",
+            "grunt-contrib-jasmine": "^0.6.5",
+            "grunt-contrib-less": "^0.11.1",
+            "grunt-contrib-uglify": "~0.2.7",
+            "grunt-contrib-watch": "~0.5.3"
+        };
+
+        // Generate package.json file, used by npm and grunt.
+        init.writePackageJSON('package.json', {
+            name: props.name,
+            description: props.description,
+            version: props.version,
+            node_version: '>= 0.10.0',
+            title: props.title,
+            devDependencies: devDependencies
+        });
+        // generate bower.json
+        init.writePackageJSON('bower.json', {
+            name: props.name,
+            description: props.description,
+            version: props.version,
+            title: props.title,
+            devDependencies: {
+                "bootstrap": "~3.1.1",
+                "vue": "~0.10.5",
+                "grunt": "~0.4.5"
+            }
+        });
 
 
-    // If is package_json true, generate package.json
-    if (props.package_json) {
-      var devDependencies = {
-        'grunt': '~0.4.2',
-        'grunt-contrib-jshint': '~0.7.2',
-        'grunt-contrib-watch': '~0.5.3'
-      };
-
-      if (props.dom) {
-        devDependencies['grunt-contrib-qunit'] = '~0.3.0';
-      } else {
-        devDependencies['grunt-contrib-nodeunit'] = '~0.2.2';
-      }
-
-      if (props.min_concat) {
-        devDependencies['grunt-contrib-concat'] = '~0.3.0';
-        devDependencies['grunt-contrib-uglify'] = '~0.2.7';
-      }
-
-      // Generate package.json file, used by npm and grunt.
-      init.writePackageJSON('package.json', {
-        node_version: '>= 0.10.0',
-        devDependencies: devDependencies
-      });
-    }
-
-    // All done!
-    done();
-  });
+        // All done!
+        done();
+    });
 
 };
